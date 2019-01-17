e6d08c0b0cbc041a231dc7907a34d873e235f13c
Chief(Okada) <ghjk1@live.jp>
2019-01-17 16:25:56 +0900

    新たなCSV読み込み関数を追加
    敵の出現タイムなど

--- Cont/Shootingood/CSVData.h
+++ Cont/Shootingood/CSVData.h
@@ -1,6 +1,7 @@
 #pragma once 
 #include<string>
 #include<map>
+#include<list>
 #include"CSV_Reader.h"
 
 class CSVData{
@@ -14,12 +15,22 @@ public:
 		std::string FilePath;
 		int			SHandle;
 	};
+	
+	struct m_EnemyArriveList
+	{
+		int			TIME;
+		int			STAGE;
+	};
 //CSVデータの格納先
 	std::map<int, m_SoundFile> m_sounds;
+	std::list<m_EnemyArriveList> m_stage1;
 	//CSVDataのコンストラクタが動いた時Load処理を開始するのでprivateにしておく
 	//構造体もprivateでいいかもしれない。
 private:
 	CSV_Reader m_reader; //TODO::とりあえずここに
 //CSVデータロード関数
+	//サウンドデータをメモリにロード
 	void LoadSound();
+	//敵の出現リストをロードする
+	void LoadEnemyArriveList();
 };
